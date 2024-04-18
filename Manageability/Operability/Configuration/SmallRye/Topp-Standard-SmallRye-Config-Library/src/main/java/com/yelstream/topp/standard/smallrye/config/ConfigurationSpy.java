a6daeb20322910a1f1876e159a07ccaec0e7d31a
Morten Sabroe Mortensen <morten.sabroe.mortensen@beumer.com>
2024-04-18 13:26:21 +0200

    Added spy-notes.

---
+++ Manageability/Operability/Configuration/SmallRye/Topp-Standard-SmallRye-Config-Library/src/main/java/com/yelstream/topp/standard/smallrye/config/ConfigurationSpy.java
@@ -0,0 +1,29 @@
+package com.yelstream.topp.standard.smallrye.config;
+
+import io.smallrye.config.SmallRyeConfig;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+
+public class ConfigurationSpy {
+
+
+    public static void bruteForcePrint() {
+
+        Config config = ConfigProvider.getConfig();
+        SmallRyeConfig smallRyeConfig = config.unwrap(SmallRyeConfig.class);
+
+        System.out.println("Profiles: "+smallRyeConfig.getProfiles());
+
+        int index=0;
+        for (var configSource: smallRyeConfig.getConfigSources()) {
+            System.out.format("[%s] ConfigSource: %n",index);
+            System.out.println("    Name:           "+configSource.getName());
+            System.out.println("    Class:          "+configSource.getClass());
+            System.out.println("    Ordinal:        "+configSource.getOrdinal());
+            System.out.println("    Property names: "+configSource.getPropertyNames());
+            System.out.println("    Property:       "+configSource.getProperties());
+            index++;
+        }
+    }
+
+}
