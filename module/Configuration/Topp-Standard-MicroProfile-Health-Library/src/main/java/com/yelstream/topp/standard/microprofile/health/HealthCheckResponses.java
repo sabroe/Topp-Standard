d7782e793e55cab31c86d9b4c70002c60800143e
Morten Sabroe Mortensen <morten.sabroe.mortensen@gmail.com>
2024-06-22 23:52:45 +0200

    Added health-check builder.

---
+++ module/Configuration/Topp-Standard-MicroProfile-Health-Library/src/main/java/com/yelstream/topp/standard/microprofile/health/HealthCheckResponses.java
@@ -0,0 +1,153 @@
+/*
+ * Project: Topp Standard
+ * GitHub: https://github.com/sabroe/Topp-Standard
+ *
+ * Copyright 2024 Morten Sabroe Mortensen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yelstream.topp.standard.microprofile.health;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Utility addressing instances of {@link HealthCheckResponse}.
+ *
+ * @author Morten Sabroe Mortensen
+ * @version 1.0
+ * @since 2024-06-22
+ */
+@Slf4j
+@UtilityClass
+public class HealthCheckResponses {
+    /**
+     * Converts a health-check response to a builder.
+     * @param response Health-check response.
+     * @return Created builder.
+     */
+    public static Builder toBuilder(HealthCheckResponse response) {
+        return Builder.fromResponse(response);
+    }
+
+    /**
+     * Creates a health-check response.
+     * @param name Name of response.
+     * @param status Status of response.
+     * @param data Data associated with response.
+     * @return Created response.
+     */
+    @SuppressWarnings("unused")
+    @lombok.Builder(builderClassName="Builder")
+    private static HealthCheckResponse createHealthCheckResponse(String name,
+                                                                 HealthCheckResponse.Status status,
+                                                                 @lombok.Singular("data") Map<String,Object> data) {
+        return new HealthCheckResponse(name,status,Optional.ofNullable(data));
+    }
+
+    /**
+     * Builder of {@link HealthCheckResponse} instances.
+     * <p>
+     *     The presence of this is a reaction against the original MicroProfile Health {@link HealthCheckResponseBuilder},
+     *     which is a bit too blunt and without nuances in its way of setting data.
+     * </p>
+     * <p>
+     *     Note that is actually extends and implements the original MicroProfile Health {@link HealthCheckResponseBuilder}.
+     * </p>
+     */
+    @SuppressWarnings({"java:S1068","FieldMayBeFinal","FieldCanBeLocal","unused","java:S1450"})
+    public static class Builder extends HealthCheckResponseBuilder {
+        /**
+         * Name of health-check response.
+         */
+        private String name;
+
+        /**
+         * Status of health-check response.
+         */
+        private HealthCheckResponse.Status status;
+
+        /**
+         * Data associated with health-check response.
+         */
+        private Map<String,Object> data=new HashMap<>();
+
+        /**
+         * Sets the response status.
+         * @param status Status.
+         * @return Builder.
+         */
+        public Builder status(HealthCheckResponse.Status status) {
+            this.status=status;
+            return this;
+        }
+
+        @Override
+        public Builder status(boolean up) {
+            return status(up?HealthCheckResponse.Status.UP:HealthCheckResponse.Status.DOWN);
+        }
+
+        @Override
+        public Builder down() {
+            return status(HealthCheckResponse.Status.DOWN);
+        }
+
+        @Override
+        public Builder up() {
+            return status(HealthCheckResponse.Status.UP);
+        }
+
+        @Override
+        public Builder withData(String key,
+                                String value) {
+            return data(key,value);
+        }
+
+        @Override
+        public Builder withData(String key,
+                                long value) {
+            return data(key,value);
+        }
+
+        @Override
+        public Builder withData(String key,
+                                boolean value) {
+            return data(key,value);
+        }
+
+        /**
+         * Creates a builder from a health-check response.
+         * @param response Health-check response.
+         * @return Created builder.
+         */
+        public static Builder fromResponse(HealthCheckResponse response) {
+            return builder().name(response.getName()).status(response.getStatus()).data(response.getData().orElse(null));
+        }
+
+        /**
+         * Creates a builder from a health-check response builder.
+         * @param builder Health-check response builder.
+         * @return Created builder.
+         */
+        public static Builder fromResponseBuilder(HealthCheckResponseBuilder builder) {
+            return fromResponse(builder.build());
+        }
+    }
+}
