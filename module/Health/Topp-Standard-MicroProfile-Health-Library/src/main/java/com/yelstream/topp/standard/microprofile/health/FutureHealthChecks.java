a43733201d6b36a2c69b43ee93d7636575c8398e
Morten Sabroe Mortensen <morten.sabroe.mortensen@gmail.com>
2024-06-24 01:31:20 +0200

    Modified helpers.

--- module/Health/Topp-Standard-MicroProfile-Health-Library/src/main/java/com/yelstream/topp/standard/microprofile/health/FutureHealthChecks.java
+++ module/Health/Topp-Standard-MicroProfile-Health-Library/src/main/java/com/yelstream/topp/standard/microprofile/health/FutureHealthChecks.java
@@ -1,13 +1,17 @@
 package com.yelstream.topp.standard.microprofile.health;
 
+import com.yelstream.topp.standard.lang.annotation.Annotations;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.Liveness;
+import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.health.Startup;
 
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CompletableFuture;
 
 /**
- * Utility addressing instances of {@link HealthCheck}.
+ * Utility addressing instances of {@link FutureHealthCheck}.
  *
  * @author Morten Sabroe Mortensen
  * @version 1.0
@@ -17,23 +21,74 @@ import java.util.concurrent.ExecutionException;
 @UtilityClass
 public class FutureHealthChecks {
     /**
-     * Converts an asynchronous health-check to a synchronous health-check.
+     * Creates a new asynchronous health-check.
      * <p>
      *     Note that the created health-check does not carry any annotations indicating the probe type.
      * </p>
-     * @param healthCheck Asynchronous health-check.
-     * @return Synchronous health-check.
+     * @param healthCheck Synchronous health-check.
+     * @return Asynchronous health-check.
      */
-    public static HealthCheck toHealthCheck(FutureHealthCheck healthCheck) {
-        return () -> {
-            try {
-                return healthCheck.submitCall().get();
-            } catch (InterruptedException ex) {
-                Thread.currentThread().interrupt();
-                throw new IllegalStateException("Failure to convert health-check type!",ex);
-            } catch (ExecutionException ex) {
-                throw new IllegalStateException("Failure to convert health-check type!",ex);
+    public static FutureHealthCheck create(HealthCheck healthCheck) {
+        return () -> CompletableFuture.supplyAsync(healthCheck::call);
+    }
+
+    /**
+     * Creates a new asynchronous health-check.
+     * <p>
+     *     This contains a probe annotation if the argument health-check contains a probe annotation.
+     * </p>
+     * @param healthCheck Synchronous health-check.
+     * @return Asynchronous health-check.
+     */
+    public static FutureHealthCheck createWithProbeAnnotation(HealthCheck healthCheck) {
+        return addProbeAnnotation(create(healthCheck));
+    }
+
+    /**
+     * Convert a health-check.
+     * <p>
+     *     This contains a probe annotation if the argument health-check contains a probe annotation.
+     * </p>
+     * @param healthCheck Health-check.
+     * @return Health-check.
+     */
+    public static FutureHealthCheck addProbeAnnotation(FutureHealthCheck healthCheck) {
+        FutureHealthCheck result=healthCheck;
+        if (Annotations.hasAnnotation(healthCheck,Liveness.class)) {
+            result=withLiveness(result);
+        } else {
+            if (Annotations.hasAnnotation(healthCheck,Startup.class)) {
+                result=withStartup(result);
+            } else {
+                if (Annotations.hasAnnotation(healthCheck,Readiness.class)) {
+                    result=withReadiness(result);
+                }
             }
-        };
+        }
+        return result;
+    }
+
+    private static LivenessFutureHealthCheck withLiveness(FutureHealthCheck healthCheck) {
+        return healthCheck::submitCall;
+    }
+
+    private static StartupFutureHealthCheck withStartup(FutureHealthCheck healthCheck) {
+        return healthCheck::submitCall;
+    }
+
+    private static ReadinessFutureHealthCheck withReadiness(FutureHealthCheck healthCheck) {
+        return healthCheck::submitCall;
+    }
+
+    @Liveness
+    private interface LivenessFutureHealthCheck extends FutureHealthCheck {
+    }
+
+    @Startup
+    private interface StartupFutureHealthCheck extends FutureHealthCheck {
+    }
+
+    @Readiness
+    private interface ReadinessFutureHealthCheck extends FutureHealthCheck {
     }
 }
--- module/Health/Topp-Standard-MicroProfile-Health-Library/src/main/java/com/yelstream/topp/standard/microprofile/health/HealthChecks.java
+++ module/Health/Topp-Standard-MicroProfile-Health-Library/src/main/java/com/yelstream/topp/standard/microprofile/health/HealthChecks.java
@@ -1,5 +1,6 @@
 package com.yelstream.topp.standard.microprofile.health;
 
+import com.yelstream.topp.standard.lang.annotation.Annotations;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.health.HealthCheck;
@@ -9,7 +10,7 @@ import org.eclipse.microprofile.health.Startup;
 
 import java.lang.annotation.Annotation;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Utility addressing instances of {@link HealthCheck}.
@@ -27,14 +28,83 @@ public class HealthChecks {
     public static final List<Class<? extends Annotation>> DEFAULT_PROBE_ANNOTATIONS=List.of(Liveness.class, Readiness.class, Startup.class);
 
     /**
-     * Converts a synchronous health-check to an asynchronous health-check.
+     * Creates a new synchronous health-check.
      * <p>
      *     Note that the created health-check does not carry any annotations indicating the probe type.
      * </p>
-     * @param healthCheck Synchronous health-check.
-     * @return Asynchronous health-check.
+     * @param healthCheck Asynchronous health-check.
+     * @return Synchronous health-check.
      */
-    public static FutureHealthCheck toFutureHealthCheck(HealthCheck healthCheck) {
-        return () -> CompletableFuture.supplyAsync(healthCheck::call);
+    public static HealthCheck create(FutureHealthCheck healthCheck) {
+        return () -> {
+            try {
+                return healthCheck.submitCall().get();
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+                throw new IllegalStateException("Failure to convert health-check type!",ex);
+            } catch (ExecutionException ex) {
+                throw new IllegalStateException("Failure to convert health-check type!",ex);
+            }
+        };
+    }
+
+    /**
+     * Creates a new synchronous health-check.
+     * <p>
+     *     This contains a probe annotation if the argument health-check contains a probe annotation.
+     * </p>
+     * @param healthCheck Asynchronous health-check.
+     * @return Synchronous health-check.
+     */
+    public static HealthCheck createWithProbeAnnotation(FutureHealthCheck healthCheck) {
+        return addProbeAnnotation(create(healthCheck));
+    }
+
+    /**
+     * Convert a health-check.
+     * <p>
+     *     This contains a probe annotation if the argument health-check contains a probe annotation.
+     * </p>
+     * @param healthCheck Health-check.
+     * @return Health-check.
+     */
+    public static HealthCheck addProbeAnnotation(HealthCheck healthCheck) {
+        HealthCheck result=healthCheck;
+        if (Annotations.hasAnnotation(healthCheck,Liveness.class)) {
+            result=withLiveness(result);
+        } else {
+            if (Annotations.hasAnnotation(healthCheck,Startup.class)) {
+                result=withStartup(result);
+            } else {
+                if (Annotations.hasAnnotation(healthCheck,Readiness.class)) {
+                    result=withReadiness(result);
+                }
+            }
+        }
+        return result;
+    }
+
+    private static LivenessHealthCheck withLiveness(HealthCheck healthCheck) {
+        return healthCheck::call;
+    }
+
+    private static StartupHealthCheck withStartup(HealthCheck healthCheck) {
+        return healthCheck::call;
+    }
+
+    private static ReadinessHealthCheck withReadiness(HealthCheck healthCheck) {
+        return healthCheck::call;
+    }
+
+    @Liveness
+    private interface LivenessHealthCheck extends HealthCheck {
+    }
+
+    @Startup
+    private interface StartupHealthCheck extends HealthCheck {
+    }
+
+    @Readiness
+    private interface ReadinessHealthCheck extends HealthCheck {
     }
 }
