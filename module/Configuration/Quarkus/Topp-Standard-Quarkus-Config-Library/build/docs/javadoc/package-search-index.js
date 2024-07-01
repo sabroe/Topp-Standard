1e8ac0a98c2b71230a8eabb410eed93686ffec40
Morten Sabroe Mortensen <morten.sabroe.mortensen@beumer.com>
2024-07-01 09:32:00 +0200

    Removed erroneous files.

--- module/Configuration/Quarkus/Topp-Standard-Quarkus-Config-Library/build/docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.yelstream.topp.standard.quarkus.config.option"}];updateSearchResults();
--- module/Configuration/SmallRye/Topp-Standard-SmallRye-Config-Library/build/docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.yelstream.topp.standard.smallrye.config"},{"l":"com.yelstream.topp.standard.smallrye.config.source.factory"},{"l":"com.yelstream.topp.standard.smallrye.config.source.factory.instance"}];updateSearchResults();
