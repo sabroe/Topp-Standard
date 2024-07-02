cb292be83e8633b628868f3fbad65e051c249ce2
Morten Sabroe Mortensen <morten.sabroe.mortensen@gmail.com>
2024-07-02 18:06:41 +0200

    WIP.

--- module/Topp-Standard-Core-Library/src/main/java/com/yelstream/topp/standard/net/ServerSockets.java
+++ module/Topp-Standard-Core-Library/src/main/java/com/yelstream/topp/standard/net/ServerSockets.java
@@ -1,30 +1,124 @@
+/*
+ * Project: Topp Standard
+ * GitHub: https://github.com/sabroe/Topp-Standard
+ *
+ * Copyright 2024 Morten Sabroe Mortensen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.yelstream.topp.standard.net;
+
+import lombok.experimental.UtilityClass;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.SocketAddress;
 
+/**
+ * Utility addressing instances of {@link ServerSocket}.
+ *
+ * @author Morten Sabroe Mortensen
+ * @version 1.0
+ * @since 2024-07-02
+ */
+@UtilityClass
 public class ServerSockets {
-
-
-
+    /**
+     * Abstract operation to be applied upon a server-socket.
+     */
     @FunctionalInterface
-    public interface BindOperation {  //ServerSocketOperation?
-        void bind(ServerSocket serverSocket) throws IOException;
+    public interface ServerSocketOperation {
+        /**
+         * Applies the operation upon a server-socket.
+         * @param serverSocket Server-socket.
+         * @throws IOException Thrown in case of I/O error.
+         */
+        void apply(ServerSocket serverSocket) throws IOException;
     }
 
-    public static void testBind(BindOperation bind) throws IOException {
-        try (ServerSocket serverSocket=new ServerSocket()) {
-            bind.bind(serverSocket);
+    /**
+     * Bind-specific operation.
+     */
+    @FunctionalInterface
+    public interface BindOperation extends ServerSocketOperation {
+        /**
+         * Binds a server-socket.
+         * @param serverSocket Server-socket.
+         * @throws IOException Thrown in case of I/O error.
+         */
+        void bind(ServerSocket serverSocket) throws IOException;
+
+        @Override
+        default void apply(ServerSocket serverSocket) throws IOException {
+            bind(serverSocket);
         }
     }
 
+    /**
+     * Tests if a socket can be bound.
+     * <p>
+     *     This is intended for probing an endpoint.
+     * </p>
+     * @param operation Bind-operation.
+     * @throws IOException Thrown in case of I/O error.
+     *                     This is thrown if the socket cannot be bound.
+     */
+    public static void testBind(BindOperation operation) throws IOException {
+        try (ServerSocket serverSocket=new ServerSocket()) {
+            operation.bind(serverSocket);
+        }
+    }
+
+    /**
+     * Tests if a socket can be bound.
+     * <p>
+     *     This is intended for probing an endpoint.
+     * </p>
+     * @param port Local port.
+     * @throws IOException Thrown in case of I/O error.
+     *                     This is thrown if the socket cannot be bound.
+     */
     public static void testBind(int port) throws IOException {
         testBind(serverSocket -> serverSocket.bind(new InetSocketAddress("localhost",port)));
     }
 
+    /**
+     * Tests if a socket can be bound.
+     * <p>
+     *     This is intended for probing an endpoint.
+     * </p>
+     * @param endpoint Endpoint to bind to.
+     * @throws IOException Thrown in case of I/O error.
+     *                     This is thrown if the socket cannot be bound.
+     */
+    public static void testBind(SocketAddress endpoint) throws IOException {
+        testBind(serverSocket -> serverSocket.bind(endpoint));
+    }
 
-
-
-
+    /**
+     * Tests if a socket can be bound.
+     * <p>
+     *     This is intended for probing an endpoint.
+     * </p>
+     * @param endpoint Endpoint to bind to.
+     * @param backlog Requested maximum length of the queue of incoming connections.
+     * @throws IOException Thrown in case of I/O error.
+     *                     This is thrown if the socket cannot be bound.
+     */
+    public static void testBind(SocketAddress endpoint,
+                                int backlog) throws IOException {
+        testBind(serverSocket -> serverSocket.bind(endpoint,backlog));
+    }
 }
--- module/Topp-Standard-Core-Library/src/main/java/com/yelstream/topp/standard/net/Sockets.java
+++ module/Topp-Standard-Core-Library/src/main/java/com/yelstream/topp/standard/net/Sockets.java
@@ -1,24 +1,159 @@
+/*
+ * Project: Topp Standard
+ * GitHub: https://github.com/sabroe/Topp-Standard
+ *
+ * Copyright 2024 Morten Sabroe Mortensen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.yelstream.topp.standard.net;
+
+import lombok.AllArgsConstructor;
+import lombok.experimental.UtilityClass;
 
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
+/**
+ * Utility addressing instances of {@link Socket}.
+ *
+ * @author Morten Sabroe Mortensen
+ * @version 1.0
+ * @since 2024-07-02
+ */
+@UtilityClass
 public class Sockets {
-
-
+    /**
+     * Abstract operation to be applied upon a socket.
+     */
     @FunctionalInterface
-    public interface ConnectOperation {  //SocketOperation?
-        void bind(Socket socket) throws IOException;
+    public interface SocketOperation {
+        /**
+         * Applies the operation upon a socket.
+         * @param socket Socket.
+         * @throws IOException Thrown in case of I/O error.
+         */
+        void apply(Socket socket) throws IOException;
     }
 
+    /**
+     * Connect-specific operation.
+     */
+    @FunctionalInterface
+    public interface ConnectOperation extends SocketOperation {
+        /**
+         * Connects a socket.
+         * @param socket Socket.
+         * @throws IOException Thrown in case of I/O error.
+         */
+        void connect(Socket socket) throws IOException;
 
-    public static void testConnectToSocket(SocketAddress socketAddress,
-                                           Duration connectTimeout) throws IOException {
-        try (Socket socket=new Socket()) {
-            socket.connect(socketAddress,(int)connectTimeout.toMillis());
+        @Override
+        default void apply(Socket socket) throws IOException {
+            connect(socket);
         }
     }
+
+    /**
+     * Tests if a socket can be connected.
+     * <p>
+     *     This is intended for probing an endpoint.
+     * </p>
+     * @param operation Connect-operation.
+     * @throws IOException Thrown in case of I/O error.
+     *                     This is thrown if the socket cannot be connected.
+     */
+    public static void testConnect(ConnectOperation operation) throws IOException {
+        try (Socket socket=new Socket()) {
+            operation.connect(socket);
+        }
+    }
+
+    /**
+     * Tests if a socket can be connected.
+     * <p>
+     *     This is intended for probing an endpoint.
+     * </p>
+     * @param endpoint Endpoint to connect to.
+     * @param connectTimeout Connect timeout.
+     * @throws IOException Thrown in case of I/O error.
+     *                     This is thrown if the socket cannot be connected.
+     */
+    public static void testConnect(SocketAddress endpoint,
+                                   Duration connectTimeout) throws IOException {
+        testConnect(socket -> socket.connect(endpoint,(int)connectTimeout.toMillis()));
+    }
+
+    /**
+     * Tests if a socket can be connected.
+     * <p>
+     *     This is intended for probing an endpoint.
+     * </p>
+     * @param endpoint Endpoint to connect to.
+     * @throws IOException Thrown in case of I/O error.
+     *                     This is thrown if the socket cannot be connected.
+     */
+    public static void testConnect(SocketAddress endpoint) throws IOException {
+        testConnect(socket -> socket.connect(endpoint));
+    }
+
+
+
+    public static <R> CompletableFuture<R> testConnect(Supplier<R> operation,
+                                                       Executor executor) {
+        return CompletableFuture.supplyAsync(operation,executor);
+    }
+
+
+
+
+
+/*
+    @lombok.Builder(builderClassName = "Builder")
+    @AllArgsConstructor
+    public static class SocketConnectivity {
+        private final SocketAddress socketAddress;
+        private final Duration connectTimeout;
+        private final Exception exception;
+    }
+
+    public static SocketConnectivity canConnectToSocket(ConnectOperation operation) {
+        SocketConnectivity.Builder builder=SocketConnectivity.builder();
+        builder.socketAddress(socketAddress);
+        builder.connectTimeout(connectTimeout);
+        try {
+            operation.apply();
+        } catch (IOException ex) {
+            builder.exception(ex);
+        }
+        return builder.build();
+    }
+
+
+
+
+    public static CompletableFuture<SocketConnectivity> testConnect(ConnectOperation operation,
+                                                       Executor executor) {
+        return CompletableFuture.supplyAsync(operation,executor);
+    }
+*/
+
 
 }
