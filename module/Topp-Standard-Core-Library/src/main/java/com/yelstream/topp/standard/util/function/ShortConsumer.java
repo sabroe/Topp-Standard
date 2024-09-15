4cbdf74b9664c5f0c62fb27b7f9ba080587fd579
Morten Sabroe Mortensen <morten.sabroe.mortensen@gmail.com>
2024-09-15 20:53:22 +0200

    Added Space and Slide.

--- module/Topp-Standard-Core-Library/src/main/java/com/yelstream/topp/standard/util/function/ShortConsumer.java
+++ module/Topp-Standard-Core-Library/src/main/java/com/yelstream/topp/standard/util/function/ShortConsumer.java
@@ -22,23 +22,14 @@ package com.yelstream.topp.standard.util.function;
 import java.util.Objects;
 
 /**
- * Consumer of {@code boolean} primitive values.
- * <p>
- *     As of Java SE 21, a {@link java.util.function.BooleanSupplier} exists
- *     while at the same time a {@code BooleanConsumer} has yet to be added.
- * </p>
- * <p>
- *     While {@code Consumer<Boolean>} may be the same as {@code BooleanConsumer} performance-wise,
- *     it is not the same when designing APIs since there are significant differences between
- *     the object {@link Boolean} and the primitive type {@code boolean}.
- * </p>
+ * Consumer of {@code short} primitive values.
  *
  * @author Morten Sabroe Mortensen
  * @version 1.0
  * @since 2024-09-15
  */
 @FunctionalInterface
-public interface BooleanConsumer {
+public interface ShortConsumer {
     /**
      * Performs this operation on the given argument.
      * @param value Argument.
@@ -50,7 +41,7 @@ public interface BooleanConsumer {
      * @param after Operation to be performed after this operation.
      * @return Composed consumer.
      */
-    default BooleanConsumer andThen(BooleanConsumer after) {
+    default ShortConsumer andThen(ShortConsumer after) {
         Objects.requireNonNull(after);
         return (boolean t) -> { accept(t); after.accept(t); };
     }
