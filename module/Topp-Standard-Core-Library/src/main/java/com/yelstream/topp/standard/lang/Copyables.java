b21dd132a66b95955908bd3a3c53db7604723133
Morten Sabroe Mortensen <morten.sabroe.mortensen@beumer.com>
2024-10-22 15:55:53 +0200

    Added initial copyable interface.

---
+++ module/Topp-Standard-Core-Library/src/main/java/com/yelstream/topp/standard/lang/Copyables.java
@@ -0,0 +1,33 @@
+package com.yelstream.topp.standard.lang;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.UtilityClass;
+
+import java.util.function.Consumer;
+
+@UtilityClass
+public class Copyables {
+
+    @AllArgsConstructor
+    public static class DefaultCopyable<T extends Copyable<T,T>> implements Copyable<T,T> {
+        @Getter
+        private T object;
+
+        @Override
+        public T copy(Consumer<T> consumer) {
+            return object;
+        }
+    }
+
+
+    public static <T extends Copyable<T,T>> Copyable<T,T> create(T object) {
+
+        return object.copy();
+    }
+
+    public static <T> T copy(T object) {
+        return create(object).copy();
+    }
+
+}
---
+++ module/Topp-Standard-Core-Library/src/main/java/com/yelstream/topp/standard/lang/X.java
@@ -0,0 +1,18 @@
+package com.yelstream.topp.standard.lang;
+
+public class X {
+
+
+    public static void main(String[] args) {
+
+        String x="qwerty";
+
+        Copyable<String> c="";
+
+    }
+
+
+    ;
+
+
+}
